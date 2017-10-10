77e39d0317348c7d7867049096bb5381f45f5303
Ray Walker <hello@raywalker.it>
2017-10-10 12:01:13 +1100

    Note to self: don't add skip tags in merge to master

--- build.sh
+++ build.sh
@@ -112,8 +112,9 @@ envsubst "${ENVVARS_STRING}" < ${BUILD_DIR}/circleci-base/templates/Dockerfile.i
 # envsubst "${ENVVARS_STRING}" < ${BUILD_DIR}/templates/README.md.in > ${BUILD_DIR}/README.md
 
 BUILD_STRING="# ${APPLICATION_NAME}
-# Build:  ${CIRCLE_BUILD_NUM:-"test-$(git rev-parse --abbrev-ref HEAD)"}
+# Branch: ${CIRCLE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}}
 # URL:    ${CIRCLE_BUILD_URL:-"(local)"}
+# Build:  ${CIRCLE_BUILD_NUM:-"test-$(git rev-parse --abbrev-ref HEAD)"}
 # ------------------------------------------------------------------------
 #                     DO NOT MAKE CHANGES HERE
 # This file is built automatically from ./templates/Dockerfile.in
