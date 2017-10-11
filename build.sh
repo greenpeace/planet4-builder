402c33ab8c2b387aad367743bf8f35bc19f5ac15
Ray Walker <hello@raywalker.it>
2017-10-11 13:55:36 +1100

    extract environment consolidation to own file

--- build.sh
+++ build.sh
@@ -45,43 +45,8 @@ shift $(($OPTIND - 1))
 
 BUILD_DIR=$(dirname $0)
 
-# CONFIG FILE
-# Read parameters from key->value configuration files
-# Note this will override environment variables at this stage
-# @todo prioritise ENV over config file ?
-
-DEFAULT_CONFIG_FILE="${BUILD_DIR}/config.default"
-if [[ ! -f "${DEFAULT_CONFIG_FILE}" ]]
-then
-  fatal "ERROR :: Default configuration file not found: ${DEFAULT_CONFIG_FILE}"
-fi
-# shellcheck source=/dev/null
-source ${DEFAULT_CONFIG_FILE}
-
-# Read from custom config file from command line parameter
-if [ ! -z "${CONFIG_FILE}" ]; then
-  if [ ! -f "${CONFIG_FILE}" ]; then
-    fatal "ERROR: Custom config file not found: ${CONFIG_FILE}"
-  fi
-
-  echo "Reading config from: ${CONFIG_FILE}"
-
-  # https://github.com/koalaman/shellcheck/wiki/SC1090
-  # shellcheck source=/dev/null
-  source ${CONFIG_FILE}
-fi
-
-# Envsubst variable consolidation
-ACK_VERSION="${ACK_VERSION:-${DEFAULT_ACK_VERSION}}"
-APPLICATION_NAME=${APPLICATION_NAME:-${DEFAULT_APPLICATION_NAME}}
-BASE_IMAGE="${BASE_IMAGE:-${DEFAULT_BASE_IMAGE}}"
-BASE_NAMESPACE="${BASE_NAMESPACE:-${DEFAULT_BASE_NAMESPACE}}"
-BASE_TAG="${BASE_TAG:-${DEFAULT_BASE_TAG}}"
-BRANCH_NAME="${CIRCLE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
-DOCKER_COMPOSE_VERSION="${DOCKER_COMPOSE_VERSION:-${DEFAULT_DOCKER_COMPOSE_VERSION}}"
-GOOGLE_SDK_VERSION="${GOOGLE_SDK_VERSION:-${DEFAULT_GOOGLE_SDK_VERSION}}"
-IMAGE_FROM="${BASE_NAMESPACE}/${BASE_IMAGE}:${BASE_TAG}"
-IMAGE_MAINTAINER="${MAINTAINER:-${DEFAULT_MAINTAINER}}"
+# Setup environment variables
+. ./bin/env.sh
 
 # Process array of cloudbuild substitutions
 function getSubstitutions() {
