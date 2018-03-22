3c68e3052d0f847cd9df5397fce1354218d3fcb2
Ray Walker <hello@raywalker.it>
2018-03-22 12:10:41 +1100

    Update gcloud path data.

--- src/circleci-base/scripts/activate-gcloud-account.sh
+++ src/circleci-base/scripts/activate-gcloud-account.sh
@@ -14,10 +14,12 @@ then
 fi
 
 # Update gcloud sdk components
-${HOME}/google-cloud-sdk/bin/gcloud --quiet components update
+gcloud --quiet components update
 # Decode base64-encoded service key json
 echo "${GCLOUD_SERVICE_KEY}" | base64 --decode -i > ${HOME}/gcloud-service-key.json
+
 # Configure project
-${HOME}/google-cloud-sdk/bin/gcloud config set project ${GOOGLE_PROJECT_ID}
-# Authneticate
-${HOME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud config set project ${GOOGLE_PROJECT_ID}
+
+# Authenticate
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
