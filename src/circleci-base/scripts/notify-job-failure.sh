2d2668e0a77b77ac1f5c2920e37a73dbe0dfff44
Ray Walker <raymond.walker@greenpeace.org>
2018-05-08 14:29:22 +1000

    Fix path

--- src/circleci-base/scripts/notify-job-failure.sh
+++ src/circleci-base/scripts/notify-job-failure.sh
@@ -6,4 +6,4 @@ MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRAN
 MSG_LINK="${CIRCLE_BUILD_URL}" \
 MSG_TEXT="${CIRCLE_JOB}\nBuild #${CIRCLE_BUILD_NUM}\nRef: ${CIRCLE_BRANCH:-${CIRCLE_TAG}}\n:Commit: ${CIRCLE_SHA1}" \
 MSG_COLOUR="red" \
-notify-rocketchat.sh
+${HOME}/scripts/notify-rocketchat.sh
