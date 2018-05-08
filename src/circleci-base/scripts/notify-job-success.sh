c1decc5d5bf5af831745f7bf0250bfa9f4446c42
Ray Walker <raymond.walker@greenpeace.org>
2018-05-08 14:18:32 +1000

    Notify of success and failure

---
+++ src/circleci-base/scripts/notify-job-success.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+
+MSG_TYPE="${TYPE:-Job} Successful: ${CIRCLE_PROJECT_REPONAME}" \
+MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRANCH:-${CIRCLE_TAG}}" \
+MSG_LINK="${CIRCLE_BUILD_URL}" \
+MSG_TEXT="${CIRCLE_JOB}\nBuild #${CIRCLE_BUILD_NUM}\nRef: ${CIRCLE_BRANCH:-${CIRCLE_TAG}}\n:Commit: ${CIRCLE_SHA1}" \
+MSG_COLOUR="lightgreen" \
+notify-rocketchat.sh
