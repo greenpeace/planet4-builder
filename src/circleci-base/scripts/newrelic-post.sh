adba1a5f182db187839f447fce80b834c6d46c0c
Raymond Walker <raymond.walker@greenpeace.org>
2018-05-16 17:19:52 +1000

    NewRelic API helpers

---
+++ src/circleci-base/scripts/newrelic-post.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+
+endpoint=$1
+payload=$2
+
+curl -X POST "$endpoint" \
+     -H "X-Api-Key:${NEWRELIC_REST_API_KEY}" \
+     -H "Content-Type: application/json" \
+     "$payload"
