96095f7abff610098b7e8bdc78cba81e9323719a
Ray Walker <raymond.walker@greenpeace.org>
2018-06-29 21:32:50 +1000

    Commit after merge resolution.

--- src/circleci-base/scripts/release-finish.sh
+++ src/circleci-base/scripts/release-finish.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+set -x
 
 new_version=$1
 commit_message=":robot: ${2:-Automated promotion}"
 
-if ! git flow release finish $new_version --showcommands -p -m $commit_message
+git flow release finish $new_version --showcommands -p -m $commit_message 2>&1 | tee ${TMPDIR:-/tmp}/gitflow.log
+
+if grep "Fatal: There were merge conflicts" ${TMPDIR:-/tmp}/gitflow.log
 then
   # Force merge conflicts to be --ours
   grep -lr '<<<<<<<' . | xargs git checkout --ours
   git add .
+  git commit -m ":robot: Resolve merge conflicts --ours"
   git flow release finish $new_version --showcommands -p -m $commit_message
 fi
