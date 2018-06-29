cef5a26091d661781033fa76d7e8339f10973009
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-29 23:29:48 +1000

    Notify of hold job that requires attention.

---
+++ src/circleci-base/scripts/notify-job-hold.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+MSG_TYPE="FAILURE: ${TYPE:-Job} - ${CIRCLE_PROJECT_REPONAME} - ${CIRCLE_JOB}" \
+MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRANCH:-${CIRCLE_TAG}}" \
+MSG_LINK="https://circleci.com/workflow-run/${CIRCLE_WORKFLOW_WORKSPACE_ID}" \
+MSG_TEXT="```
+Build:  #${CIRCLE_BUILD_NUM}
+Diff:   ${CIRCLE_COMPARE_URL}
+${EXTRA_TEXT:-}
+```" \
+MSG_COLOUR="purple" \
+"${HOME}/scripts/notify-rocketchat.sh"
