6ce333ce6ca0861d314279476ff117ba9f563289
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-30 00:13:15 +1000

    Notification text tweaks. Again.

--- src/circleci-base/scripts/notify-job-hold.sh
+++ src/circleci-base/scripts/notify-job-hold.sh
@@ -4,7 +4,6 @@ set -eu
 MSG_TYPE="HOLD: ${TYPE:-Job} - ${CIRCLE_PROJECT_REPONAME} - ${CIRCLE_JOB}" \
 MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRANCH:-${CIRCLE_TAG}}" \
 MSG_LINK="https://circleci.com/workflow-run/${CIRCLE_WORKFLOW_WORKSPACE_ID}" \
-MSG_TEXT="`Build:  #${CIRCLE_BUILD_NUM}` `Diff:   ${CIRCLE_COMPARE_URL}`
-${EXTRA_TEXT:-}" \
+MSG_TEXT="Build: #${CIRCLE_BUILD_NUM} Diff: ${CIRCLE_COMPARE_URL} ${EXTRA_TEXT:-}" \
 MSG_COLOUR="#ab7fd1" \
 "${HOME}/scripts/notify-rocketchat.sh"
