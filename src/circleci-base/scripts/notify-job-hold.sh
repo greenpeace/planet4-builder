013ddf2e18d6d61c62aa7adcc417efca8e1aa420
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-30 00:12:04 +1000

    Set CircleCI hold colour

--- src/circleci-base/scripts/notify-job-hold.sh
+++ src/circleci-base/scripts/notify-job-hold.sh
@@ -6,5 +6,5 @@ MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRAN
 MSG_LINK="https://circleci.com/workflow-run/${CIRCLE_WORKFLOW_WORKSPACE_ID}" \
 MSG_TEXT="`Build:  #${CIRCLE_BUILD_NUM}` `Diff:   ${CIRCLE_COMPARE_URL}`
 ${EXTRA_TEXT:-}" \
-MSG_COLOUR="purple" \
+MSG_COLOUR="#ab7fd1" \
 "${HOME}/scripts/notify-rocketchat.sh"
