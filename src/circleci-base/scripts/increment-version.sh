ca9a579876742b32094ac2468d88703580d1ea40
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-29 20:21:04 +1000

    Docs

--- src/circleci-base/scripts/increment-version.sh
+++ src/circleci-base/scripts/increment-version.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Takes a semantic version number as argument and increments the trailing value
+#
+# eg: v1.0.0 => v1.0.1
+#     1.2.9  => 1.2.10
+#     1.5    => 1.6
+#     99     => 100
+
 perl -pe 's/^(v?(\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' <<< $1
