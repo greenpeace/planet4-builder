cdd2eaace23074383e4273b6dde84f882faab9f8
Raymond Walker <raymond.walker@greenpeace.org>
2018-06-29 21:17:10 +1000

    Configure git and show commands in release-start.sh

--- src/circleci-base/scripts/release-start.sh
+++ src/circleci-base/scripts/release-start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Configure git user
+git config user.email "circleci-bot@greenpeace.org"
+git config user.name "CircleCI Bot"
+git config push.default simple
+
 git flow init -d
 
-git flow release start $1
+git flow release start --showcommands $1
