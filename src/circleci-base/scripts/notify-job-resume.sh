23f455d17c6da04eca59247d9480ede703de74aa
Ray Walker <raymond.walker@greenpeace.org>
2018-07-09 15:37:40 +1000

    Add job resume notification script.

---
+++ src/circleci-base/scripts/notify-job-resume.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+
+MSG_TYPE="RESUME: ${TYPE:-Job} - ${CIRCLE_PROJECT_REPONAME} - ${CIRCLE_JOB}" \
+MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRANCH:-${CIRCLE_TAG}}" \
+MSG_LINK="https://circleci.com/workflow-run/${CIRCLE_WORKFLOW_WORKSPACE_ID}" \
+MSG_TEXT="Build: #${CIRCLE_BUILD_NUM} ${EXTRA_TEXT:-}" \
+MSG_COLOUR="lightblue" \
+"${HOME}/scripts/notify-rocketchat.sh"
