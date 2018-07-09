4312ced29942208f12554f5a595370fe71ab630b
Ray Walker <raymond.walker@greenpeace.org>
2018-07-09 13:00:28 +1000

    Remove unnecessary paths

--- src/circleci-base/scripts/trigger-build.sh
+++ src/circleci-base/scripts/trigger-build.sh
@@ -20,9 +20,9 @@ git clone $repo $dir && \
   git checkout $branch && \
   git commit --allow-empty -m ":robot: Build trigger from ${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} Build #${CIRCLE_BUILD_NUM}" -m "${CIRCLE_BUILD_URL}" && \
   git push --force-with-lease --set-upstream origin $branch && \
-  ${HOME}/scripts/notify-job-success.sh && \
+  notify-job-success.sh && \
   exit 0
 
 # No bueno
-${HOME}/scripts/notify-job-failure.sh
+notify-job-failure.sh
 exit 1
