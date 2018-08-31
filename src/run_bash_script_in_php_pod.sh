04992fc89deddccf0209adb1786b7903b242a056
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-08-31 16:25:29 +0300

    Work in progress. DO NOT MERGE
    Non tested, and very possibly breaking everything. Includes: 
    - A meta script run_bash_script_in_php_pod.sh allowing us to run other bash scripts in the kubernetes pod php of given deploy
    - A test bash script ls.sh  that just lists the current directory (so that we can test if the metascript works)
    - A wp script modify_users.sh that adds missing users to a deployed wp installation (not used yet)
    - A modification in the Makefile where we try to run the script ls.sh after a deploy.

---
+++ src/run_bash_script_in_php_pod.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu
+
+external_script=$1
+
+php=$(kubectl get pods --namespace "${HELM_NAMESPACE}" -l "app=wordpress-php,release=${HELM_RELEASE}" -o jsonpath="{.items[0].metadata.name}")
+
+if [[ -z "$php" ]]
+then
+  >&2 echo "ERROR: php pod not found in release ${HELM_RELEASE}"
+fi
+
+kubectl cp $external_script $php:/app/source/public/$external_script
+kubectl exec ${HELM_NAMESPACE} $php -- chown app:app /app/source/public/$external_script
+kubectl exec ${HELM_NAMESPACE} $php -- chmod 744 /app/source/public/$external_script
+kubectl exec ${HELM_NAMESPACE} $php bash /app/source/public/$external_script
+kubectl exec ${HELM_NAMESPACE} $php -- rm /app/source/public/$external_script
