6cfef4d20ef76d091c86da4ac963b6850fa5120e
Ray Walker <raymond.walker@greenpeace.org>
2018-11-01 15:07:11 +1100

    Double quote

--- src/run_bash_script_in_php_pod.sh
+++ src/run_bash_script_in_php_pod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-external_script=$1
+external_script="$1"
 
 php=$(kubectl get pods --namespace "${HELM_NAMESPACE}" --field-selector=status.phase=Running -l "app=wordpress-php,release=${HELM_RELEASE}" -o jsonpath="{.items[0].metadata.name}")
 
@@ -10,6 +10,11 @@ then
   >&2 echo "ERROR: php pod not found in release ${HELM_RELEASE}"
 fi
 
-kubectl -n ${HELM_NAMESPACE} cp $external_script $php:/app/bin/$external_script
-kubectl -n ${HELM_NAMESPACE} exec $php bash /app/bin/$external_script
-kubectl -n ${HELM_NAMESPACE} exec $php rm /app/bin/$external_script
+if [[ ! -e "$1" ]]
+then
+  >&2 echo "ERROR: file does not exist: '$1'"
+fi
+
+kubectl -n "${HELM_NAMESPACE}" cp "$external_script" "$php:/app/bin/$external_script"
+kubectl -n "${HELM_NAMESPACE}" exec "$php" bash "/app/bin/$external_script"
+kubectl -n "${HELM_NAMESPACE}" exec "$php" rm "/app/bin/$external_script"
