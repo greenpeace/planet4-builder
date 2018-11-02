400046920087d221debf1aff4428c75629d6791f
Ray Walker <raymond.walker@greenpeace.org>
2018-11-02 13:40:33 +1100

    Make bash executor script a little more functional and robust. - Now passes any parameters to script - Checks for existence of script - Copies file to /tmp/ dir instead of /app/bin

--- src/run_bash_script_in_php_pod.sh
+++ src/run_bash_script_in_php_pod.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-set -exu
+set -euo pipefail
 
-external_script="$1"
+external_script=$1
+shift
+args=$*
 
 php=$(kubectl get pods --namespace "${HELM_NAMESPACE}" --field-selector=status.phase=Running -l "app=wordpress-php,release=${HELM_RELEASE}" -o jsonpath="{.items[0].metadata.name}")
 
@@ -10,11 +12,11 @@ then
   >&2 echo "ERROR: php pod not found in release ${HELM_RELEASE}"
 fi
 
-if [[ ! -e "$1" ]]
+if [[ ! -e "$external_script" ]]
 then
   >&2 echo "ERROR: file does not exist: '$1'"
 fi
 
-kubectl -n "${HELM_NAMESPACE}" cp "$external_script" "$php:/app/bin/$external_script"
-kubectl -n "${HELM_NAMESPACE}" exec "$php" bash "/app/bin/$external_script"
-kubectl -n "${HELM_NAMESPACE}" exec "$php" rm "/app/bin/$external_script"
+kubectl -n "${HELM_NAMESPACE}" cp "$external_script" "$php:/tmp/$external_script"
+kubectl -n "${HELM_NAMESPACE}" exec "$php" -- "/tmp/$external_script" "$args"
+kubectl -n "${HELM_NAMESPACE}" exec "$php" -- rm "/tmp/$external_script"
