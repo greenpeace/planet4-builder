d1470061bebb31406260adc3162c7820fafc3a47
Ray Walker <raymond.walker@greenpeace.org>
2018-11-13 13:42:18 +1100

    Try to rollback to previous SUPERSEDED release

--- src/helm_rollback.sh
+++ src/helm_rollback.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 
 release=${1:-${HELM_RELEASE}}
-current=$(helm history "$release" --max=1 | tail -1 | awk '{ print $1 }' | xargs)
+helm history "$release" --max=10
+
+helm get "$release" > helm_get_release.txt
+current=$(grep '^RELEASE:' helm_get_release.txt | cut -d' ' -f2)
 
 re='^[0-9]+$'
 if ! [[ $current =~ $re ]] ; then
@@ -10,14 +13,28 @@ if ! [[ $current =~ $re ]] ; then
    exit 1
 fi
 
+helm status "$release" | tee helm_release_status.txt
+
 if [[ $current -eq 1 ]]
 then
   >&2 echo "ERROR: $release is first revision, cannot perform helm rollback"
   exit 1
 fi
 
+status=$(grep '^STATUS:' helm_release_status.txt | cut -d' ' -f2)
+echo "Release #current of $release is in state: $status"
+
+# Find last good deployment to roll back to
 previous=${2:-$((current-1))}
-echo "Helm: Rolling back $release to revision $previous"
+while [[ $previous -gt 0 ]]
+do
+  [[ $(helm status "$release" | grep '^STATUS:' | cut -d' ' -f2 | xargs) == "SUPERSEDED"  ]] && break
+  previous=${2:-$((current-1))}
+done
+
+[[ $previous -lt 1 ]] && >&2 echo "ERROR: No good releases to roll back to!" && exit 1
+
+echo "Helm: Rolling back $release to revision: $previous"
 
 if helm rollback "$release" "$previous"
 then
