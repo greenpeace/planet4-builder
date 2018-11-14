d03e482776f5126e218ca3eba02dc2ccca11311a
Ray Walker <raymond.walker@greenpeace.org>
2018-11-14 11:50:07 +1100

    Adds rollback fixes and improvements

--- src/helm_rollback.sh
+++ src/helm_rollback.sh
@@ -27,31 +27,40 @@ fi
 status=$(grep '^STATUS:' helm_release_status.txt | cut -d' ' -f2)
 echo "Release #current of $release is in state: $status"
 
-# Find last good deployment to roll back to
-previous=${2:-$((current-1))}
+# Find last good deployment
+previous=${2:-$(( current - 1 ))}
 while [[ $previous -gt 0 ]]
 do
-  [[ $(helm status "$release" | grep '^STATUS:' | cut -d' ' -f2 | xargs) == "SUPERSEDED"  ]] && break
-  previous=${2:-$((current-1))}
+  [[ $(helm status "$release" --revision "$previous" | grep '^STATUS:' | cut -d' ' -f2 | xargs) == "SUPERSEDED"  ]] && break
+  previous=$(( previous - 1 ))
 done
 
+# Nothing found, exit with error
 [[ $previous -lt 1 ]] && >&2 echo "ERROR: No good releases to roll back to!" && exit 1
 
-echo "Helm: Rolling back $release to revision: $previous"
+# Success
+function rollback() {
+  echo "Rolling back $release to revision: $previous"
 
-if helm rollback "$release" "$previous"
-then
-  helm history "$release" --max=10
+  # Perform rollback
+  if helm rollback "$release" "$previous"
+  then
+    echo "SUCCESS: Release $release now at revision: $previous"
+    TYPE="Helm Rollback" \
+    EXTRA_TEXT="\`\`\`
+    History:
+    $(helm history "${HELM_RELEASE}" --max=5)
+    \`\`\`" \
+    notify-job-success.sh
 
-  TYPE="Helm Rollback" \
-  EXTRA_TEXT="\`\`\`
-  History:
-  $(helm history "${HELM_RELEASE}" --max=5)
-  \`\`\`" \
-  notify-job-success.sh
+    return 0
+  fi
 
-  exit 0
-fi
+  >&2 echo "ERROR: Failed to rollback $release to revision: $previous"
+  return 1
+}
+
+retry rollback && exit 0
 
 >&2 echo "ERROR: Failed during rollback"
 helm status "$release
