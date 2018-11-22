b5d2b9b367c88c580da4580726ebb4914e0b2d28
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-11-22 09:15:22 +0200

    Make flush_redis run from everywhere

--- src/flush_redis.sh
+++ src/flush_redis.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 set -eu
 
+# Find real file path of current script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+# determine where the flush_redis script lives
+source="${BASH_SOURCE[0]}"
+while [[ -h "$source" ]]
+do # resolve $source until the file is no longer a symlink
+          dir="$( cd -P "$( dirname "$source" )" && pwd )"
+            source="$(readlink "$source")"
+              [[ $source != /* ]] && source="$dir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+      done
+      FLUSH_REDIS_DIR="$( cd -P "$( dirname "$source" )" && pwd )"
+      export FLUSH_REDIS_DIR
+
 # shellcheck disable=SC1091
-. lib/retry.sh
+. "${FLUSH_REDIS_DIR}/lib/retry.sh" 
 
 function flush() {
   redis=$(kubectl get pods --namespace "${HELM_NAMESPACE}" \
