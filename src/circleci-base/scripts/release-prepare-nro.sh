aa85c8196ca7f8d51d80bd3762edb17c9debed3f
Konstantinos Kokkorogiannis <kkokkoro@greenpeace.org>
2018-12-17 11:35:06 +0200

    PLANET-2967 Feature/remove more empty git commits (#13)
    Triggers promotion via API when possible, merge commits if changed content.

--- src/circleci-base/scripts/release-prepare-nro.sh
+++ src/circleci-base/scripts/release-prepare-nro.sh
@@ -13,62 +13,82 @@ set -euo pipefail
 old_release=${1:-$(git-current-tag.sh)}
 new_release=${2:-$(increment-version.sh "$old_release")}
 
+echo "--0.1 The old release is $old_release"
+echo "--0.2 The new release is $new_release"
+
 merged=false
 
 mkdir -p /tmp/workspace
 
+echo "--1.0 Before the first if"
 if release-start.sh "$new_release"
 then
+  echo "--1.1 New release branch created: release/$new_release"
   merged=true
 else
+  echo "--1.1 Release branch release/$new_release already exists"
   # Release branch already exists
   git checkout "release/$new_release"
 
   # If there are any changes from develop
   # Merge changes from develop to release
   git merge -Xtheirs --no-edit --log -m ":robot: release/$new_release Merge develop" develop | tee /tmp/workspace/merge.log
-  grep -q "Already up-to-date." /tmp/workspace/merge.log  || merged=true
+  
+  grep -q "Already up-to-date." /tmp/workspace/merge.log || {
+    echo "--1.2 We merged changes from develop into release/$new_release"
+    merged=true;
+  }
 fi
 
 # Perform NRO develop to release manipulations
+echo
+echo "---2. Performing automated release modifications ..."
 pin-composer-versions.sh
 
 # If there are any local changes
 if ! git diff --exit-code
 then
+  echo "---2.1 Staging modifications"
+  # Stage changes
   git add .
-  echo "---2. We have local changes"
-  # We have local changes
+
   if [[ "$merged" = "true" ]]
   then
-    echo "---2.1. Since we've merged changes from develop, let's amend that commit"
-    git commit --amend --no-edit --allow-empty
+    echo "---2.1.1 Since we've merged changes from develop, let's amend that commit"
+    git commit --amend --no-edit
   else
-    echo "---2.2 New commit with automated modifications"
+    echo "---2.1.2 Create new commit with automated modifications"
     git commit -m ":robot: release/$new_release Automated modifications "
-  fi
-else
-  # No local changes
-  echo "---3. No local changes"
-  if [[ "$merged" = "true" ]]
-  then
-    echo "---3.1 Nothing to do: no new changes and have already merged develop -> release in this job"
-  else
-    echo "---3.2 Creating empty commit as new build trigger ..."
-    git commit --allow-empty -m ":robot: release/$new_release Build trigger"
+    merged=true
   fi
 fi
 
-message=$(git show --format=%B | grep -v ":robot: Build trigger")
+echo "--3.0 Before the final if"
+if [[ "$merged" = "false" ]]
+then
+  # No local changes
+  repo=$(git remote get-url origin | cut -d'/' -f 2 | cut -d'.' -f1)
+  echo "---3.1 No changes to merge. Triggering $repo@release/$new_release via API"
+  trigger-build-api.sh "$repo" "release/$new_release"
+else
+  echo "---3.2 Local changes have been merged, pushing changes to remote"
 
-echo "---4. Remove all the build trigger notifications from the latest commit message"
-# Remove all the build trigger notifications from the latest commit message
-git commit --allow-empty --amend -m "$message"
+  echo "---3.2.1 Remove all the build trigger notifications from the latest commit message"
+  # Remove all the build trigger notifications from the latest commit message
+  message=$(git show --format=%B | grep -v ":robot: Build trigger")
+  git commit --amend -m "$message"
 
-echo "---5. Create the new release branch "
-# Create the new release branch
-git push -u origin "release/$new_release"
+  echo "---3.2.2 Push the new release branch release/$new_release"
+  # Push the new release branch
+  git push -u origin "release/$new_release"
 
-echo "---6. Delete the old release branch "
-# Delete the old release branch
-git push origin --delete "release/$old_release" || exit 0
+  echo "---3.2.3 Check if old release branch still exists"
+  gitlsremote=$(git ls-remote)
+  if [[ $gitlsremote =~ release/$old_release ]]
+  then
+    # Delete the old release branch
+    echo "---3.2.4 Delete stale release branch: release/$old_release"
+    git push origin --delete "release/$old_release"
+  fi
+
+fi
