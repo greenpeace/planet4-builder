4135ceaec8f966223cd809461234b2b2f5387934
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-01-18 11:24:51 +0200

    First effort to change branch of master-theme on the fly
    To be used for automatic testing.

---
+++ src/rewrite_app_repos.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+FILE=/home/circleci/source/composer-local.json
+
+if [ ! -z "$MASTER_THEME_BRANCH" ]
+then
+    echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
+    sed -i "s/\"greenpeace\/planet4-master-theme\" : \".*\",/\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",/g" ${FILE}
+else
+    echo "Nothing to replace for the master theme"
+fi
