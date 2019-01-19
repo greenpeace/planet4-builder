f1ada2343f5afcb353d9eb31bdf2629b6064b606
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-01-19 17:09:36 +0200

    Rewrite only central composer.json not the local ones

--- src/rewrite_app_repos.sh
+++ src/rewrite_app_repos.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-FILE=/home/circleci/build/source/composer-local.json
-
-if [ ! -z "$MASTER_THEME_BRANCH" ]
-then
-    echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
-    sed -i "s|\"greenpeace\/planet4-master-theme\" : \".*\",|\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" ${FILE}
-else
-    echo "Nothing to replace for the master theme"
-fi
-
-
-FILE=/home/circleci/source/composer-local.json
+FILE=/home/circleci/source/composer.json
 
 if [ ! -z "$MASTER_THEME_BRANCH" ]
 then
