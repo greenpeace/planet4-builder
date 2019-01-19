10d267a5b552815060527d7bdfecfbb620d83a5e
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-01-19 18:21:08 +0200

    Rewrite the /build/source/composer.json to test if this is picked up

--- src/rewrite_app_repos.sh
+++ src/rewrite_app_repos.sh
@@ -10,3 +10,18 @@ then
 else
     echo "Nothing to replace for the master theme"
 fi
+
+
+FILE=/home/circleci/build/source/composer.json
+
+if [ ! -z "$MASTER_THEME_BRANCH" ]
+then
+    echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
+    sed -i "s|\"greenpeace\/planet4-master-theme\" : \".*\",|\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" ${FILE}
+else
+    echo "Nothing to replace for the master theme"
+fi
+
+
+echo "DEBUG: We will echo where master theme is defined as what: "
+grep -r -H '"greenpeace/planet4-master-theme" :' *
