c5b6580d5fb5fef48a873f990642dd8056b3986d
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-01-21 11:50:07 +0200

    Put all operations for master theme in the same if, and delete cached

--- src/rewrite_app_repos.sh
+++ src/rewrite_app_repos.sh
@@ -6,18 +6,16 @@ FILE=/home/circleci/source/composer.json
 if [ ! -z "$MASTER_THEME_BRANCH" ]
 then
     echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
+
+    FILE=/home/circleci/source/composer.json
     sed -i "s|\"greenpeace\/planet4-master-theme\" : \".*\",|\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" ${FILE}
-else
-    echo "Nothing to replace for the master theme"
-fi
 
-
-FILE=/home/circleci/merge/composer.json
-
-if [ ! -z "$MASTER_THEME_BRANCH" ]
-then
-    echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
+    FILE=/home/circleci/merge/composer.json
     sed -i "s|\"greenpeace\/planet4-master-theme\" : \".*\",|\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" ${FILE}
+
+    echo "And now, delete any cached version of this package"
+    rm -rf /home/circleci/source/cache/files/greenpeace/planet4-master-theme
+
 else
     echo "Nothing to replace for the master theme"
 fi
