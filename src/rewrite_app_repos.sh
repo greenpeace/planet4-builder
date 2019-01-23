635e8872396b9400529c0f41ae7d5a1f0ac0f518
Ray Walker <raymond.walker@greenpeace.org>
2019-01-23 14:21:30 +1100

    Rewrite and fix for error in missing files.
    Fixes #13

--- src/rewrite_app_repos.sh
+++ src/rewrite_app_repos.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 set -ex
 
-FILE=/home/circleci/source/composer.json
+files=(
+  "${HOME}/source/composer.json"
+  "${HOME}/source/composer-local.json"
+  "${HOME}/merge/composer.json"
+  "${HOME}/merge/composer-local.json"
+)
 
-if [ ! -z "$MASTER_THEME_BRANCH" ]
+if [ ! -z "${MASTER_THEME_BRANCH}" ]
 then
-    echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
+  echo "Replacing master theme with branch ${MASTER_THEME_BRANCH}"
 
-    FILE=/home/circleci/source/composer.json
-    sed -i "s|\"greenpeace\/planet4-master-theme\" : \".*\",|\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" ${FILE}
+  for f in "${files[@]}"
+  do
+    if [ -e "$f" ]
+    then
+      echo " - $f"
+      sed -i "s|\"greenpeace\\/planet4-master-theme\" : \".*\",|\"greenpeace\\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" "${f}"
+    fi
+  done
 
-    FILE=/home/circleci/merge/composer.json
-    sed -i "s|\"greenpeace\/planet4-master-theme\" : \".*\",|\"greenpeace\/planet4-master-theme\" : \"${MASTER_THEME_BRANCH}\",|g" ${FILE}
-
-    echo "And now, delete any cached version of this package"
-    rm -rf /home/circleci/source/cache/files/greenpeace/planet4-master-theme
+  echo "And now, delete any cached version of this package"
+  rm -rf "${HOME}/source/cache/files/greenpeace/planet4-master-theme"
 
 else
-    echo "Nothing to replace for the master theme"
+  echo "Nothing to replace for the master theme"
 fi
 
-
 echo "DEBUG: We will echo where master theme is defined as what: "
-grep -r -H '"greenpeace/planet4-master-theme" :' *
+grep -r -H '"greenpeace/planet4-master-theme" :' ./*
