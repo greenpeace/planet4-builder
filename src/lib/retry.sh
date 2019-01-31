f4f3c12ba1480e5ef50bf046a94c74e6325315e0
Ray Walker <raymond.walker@greenpeace.org>
2019-01-31 11:05:46 +1100

    Revert most of the changes, minimal change to fix bug.

--- src/lib/retry.sh
+++ src/lib/retry.sh
@@ -12,23 +12,29 @@ function retry {
   local max_attempts=${ATTEMPTS:-5}
   local timeout=${TIMEOUT:-1}
   local attempt=1
-  local exitCode=0
+  local exitCode
 
-  while true; do
-    "$@" && break || {
-      exitCode=$?
-      echo "the exitcode is $exitCode"
-      if [[ $n -lt $max_attempts ]]; then
-        ((n++))
-        echo "Command failed. Attempt $n/$max_attempts. Retrying after $timeout seconds."
-        sleep $timeout;
-        timeout=$(( timeout * 2 ))
-      else
-        echo "The command has failed after $n attempts."
-        return $exitCode
-      fi
-    }
+  while (( attempt <= max_attempts ))
+  do
+    set +e
+    "$@"
+    exitCode=$?
+    set -e
+
+    [ $exitCode -eq 0 ] && return 0
+
+    >&2 printf "Attempt #%d/%d failed. " "$attempt" "$max_attempts"
+    attempt=$(( attempt + 1 ))
+    (( attempt > max_attempts )) && break
+
+    echo "Retrying in $timeout seconds ..."
+    sleep "$timeout"
+    timeout=$(( timeout * 2 ))
   done
+
+  >&2 echo "You've failed me for the last time! ($*)"
+
+  return $exitCode
 }
 
 export retry
