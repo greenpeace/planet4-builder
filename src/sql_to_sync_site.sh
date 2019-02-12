e41d9051f9cbdced9bc7c658c4d6b9d57298e7d8
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-02-12 10:26:40 +0200

    Add the correct files to read with yq

--- src/sql_to_sync_site.sh
+++ src/sql_to_sync_site.sh
@@ -96,8 +96,8 @@ echo "Check if we are in an pathless environment"
 echo ""
 if [[ $APP_HOSTPATH == "<nil>" ]]
 then
-   OLD_PATH=$(yq read /tmp/workspace/src/.circleci/ job_environments.production_environment.APP_HOSTNAME)
-   NEW_PATH=$(yq read /tmp/workspace/src/.circleci/ job_environments.${SITE_ENV}_environment.APP_HOSTNAME)
+   OLD_PATH=$(yq read /tmp/workspace/src/.circleci/config.yml job_environments.production_environment.APP_HOSTNAME)
+   NEW_PATH=$(yq read /tmp/workspace/src/.circleci/config.yml job_environments.${SITE_ENV}_environment.APP_HOSTNAME)
    echo ""
    echo "We are in a pathless environment. We will replace $OLD_PATH with $NEW_PATH"
    echo ""
