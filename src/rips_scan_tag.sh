84bb105345ece77251cbd074626a7fe66967f048
Pablo Cubico <pablocuadrado@gmail.com>
2019-04-30 23:32:56 -0300

    Shellcheck styling and passing app ID as parameter.

--- src/rips_scan_tag.sh
+++ src/rips_scan_tag.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# The application ID is required
+RIPS_APPLICATION_ID=$1
+
 # Default organisation is 'greenpeace'
-ORG=${1:-greenpeace}
+ORG=${2:-greenpeace}
 
 # Default repository is current repo
-REPO=${2:-$CIRCLE_PROJECT_REPONAME}
+REPO=${3:-$CIRCLE_PROJECT_REPONAME}
 
 ZIPBALL_URL=$(curl -s "https://api.github.com/repos/${ORG}/${REPO}/tags" | jq -r '.[0].zipball_url')
-wget "${ZIPBALL_URL}" -O "${REPO}.zip"
+wget "${ZIPBALL_URL}" -O "${REPO}".zip
 
 wget https://github.com/rips/rips-cli/releases/download/1.2.1/rips-cli.phar -O ./rips-cli
 chmod 755 ./rips-cli
 
-./rips-cli rips:scan:start -a ${RIPS_APPLICATION_ID} -p ./${REPO}.zip
+./rips-cli rips:scan:start -a "${RIPS_APPLICATION_ID}" -p ./"${REPO}".zip
