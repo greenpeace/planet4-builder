b35f9f4f7673d3144d99f7a9a15d0e72a653ef3c
Nikos Roussos <nikos@roussos.cc>
2020-06-12 13:22:47 +0300

    Show volunteer tickets

--- src/circleci-base/scripts/changelog.sh
+++ src/circleci-base/scripts/changelog.sh
@@ -11,7 +11,7 @@ VERSION="$1"
 function receive() {
   type=$1
 
-  JIRA_API_QUERY="https://jira.greenpeace.org/rest/api/latest/search?jql=project%20%3D%20PLANET%20AND%20fixVersion%20%3D%20${VERSION}%20AND%20type=${type}&fields=summary,issuetype,customfield_13100"
+  JIRA_API_QUERY="https://jira.greenpeace.org/rest/api/latest/search?jql=project%20%3D%20PLANET%20AND%20fixVersion%20%3D%20${VERSION}%20AND%20type=${type}&fields=summary,issuetype,customfield_13100,assignee"
 
   jira_json=$(curl -s "$JIRA_API_QUERY")
   retval=$?
@@ -22,6 +22,7 @@ function receive() {
 
   echo "$jira_json" | jq --raw-output '.issues []  | .key ' > /tmp/$$.keys
   echo "$jira_json" | jq --raw-output '.issues []  | .fields .summary ' > /tmp/$$.summaries
+  echo "$jira_json" | jq --raw-output '.issues []  | .fields .assignee .name ' > /tmp/$$.assignees
 
   keys=()
   i=0
@@ -38,6 +39,14 @@ function receive() {
     summaries[i]=$line
     i=$((i + 1))
   done < /tmp/$$.summaries
+
+  assignees=()
+  i=0
+  while read -r line
+  do
+    assignees[i]=$line
+    i=$((i + 1))
+  done < /tmp/$$.assignees
 
   total=${#keys[*]}
 
@@ -57,7 +66,13 @@ function receive() {
     do
       key=$(echo "${keys[$i]}" | xargs )
       summary="${summaries[$i]}"
-      echo "<li><a href='https://jira.greenpeace.org/browse/${key}'>${key}</a> - ${summary}</li>"
+      assignee="${assignees[$i]}"
+      volunteer_star=""
+      if [ "$assignee" == "volunteer" ]; then
+        volunteer_star=" ⭐"
+        volunteer_memo="<font size='1'>⭐ Contributed by a volunteer</font><br>"
+      fi
+      echo "<li><a href='https://jira.greenpeace.org/browse/${key}'>${key}</a> - ${summary}${volunteer_star}</li>"
     done
     echo "</ul>"
     echo
@@ -65,6 +80,7 @@ function receive() {
 }
 
 now="$(date +'%Y-%m-%d')"
+volunteer_memo=""
 
 echo
 echo "<h2>${VERSION} - ${now}</h2>"
@@ -72,3 +88,5 @@ echo
 
 receive "Task"
 receive "Bug"
+
+echo "$volunteer_memo"
