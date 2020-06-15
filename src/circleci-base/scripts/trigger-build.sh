7b01c059c07983f194f0c0a162f58e30eb4067f2
Nikos Roussos <nikos@roussos.cc>
2020-06-15 13:01:39 +0300

    PLANET-5141 Adjust trigger-build script for new release flow
    - Reverse promotion logic: auto-promote by default, use [HOLD] to block
    that
    - Commit on develop branch for develop
    - Push new tag for staging and production

--- src/circleci-base/scripts/trigger-build.sh
+++ src/circleci-base/scripts/trigger-build.sh
@@ -4,34 +4,33 @@ set -u
 repo=$1
 branch=${2:-develop}
 
-export TYPE="Trigger: $repo:$branch"
-
 dir=$(echo "$repo" | tr -dc 'a-zA-Z0-9' | head -n 1)
 
 rm -fr "$dir"
 mkdir "$dir"
 
-GIT_COMMIT_DESC=$(git --git-dir=/tmp/workspace/.git log --format=%B -n 1 "$CIRCLE_SHA1")
-if [[ $GIT_COMMIT_DESC == *"[DEV]"* ]]; then
-  GIT_PREFIX="[DEV]"
-elif [[ $GIT_COMMIT_DESC == *"[AUTO-PROCEED]"* ]]; then
-  GIT_PREFIX="[AUTO-PROCEED]"
+echo "Triggering $repo"
+
+GIT_COMMIT_MESSAGE=$(git --git-dir=/tmp/workspace/.git log --format=%B -n 1 "$CIRCLE_SHA1")
+if [[ $GIT_COMMIT_MESSAGE == *"[HOLD]"* ]]; then
+  GIT_PREFIX=" [HOLD]"
 else
   GIT_PREFIX=""
-fi;
+fi
 
 # Checkout dependent repository and trigger CI with empty commit.
-git clone "$repo" "$dir" && \
-  cd "$dir" && \
-  git config user.email "circleci-bot@greenpeace.org" && \
-  git config user.name "CircleCI Bot" && \
-  git config push.default simple && \
-  git checkout "$branch" && \
-  git commit --allow-empty -m ":robot: ${GIT_PREFIX} Build trigger from ${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} Build #${CIRCLE_BUILD_NUM}" -m "${CIRCLE_BUILD_URL}" && \
-  git push --force-with-lease --set-upstream origin "$branch" && \
-  notify-job-success.sh && \
-  exit 0
-
-# No bueno
-notify-job-failure.sh
-exit 1
+git clone "$repo" "$dir"
+cd "$dir" || exit 1
+git config user.email "circleci-bot@greenpeace.org"
+git config user.name "CircleCI Bot"
+git config push.default simple
+git checkout "$branch"
+if [ "$branch" == "develop" ]; then
+  git commit --allow-empty -m ":robot:${GIT_PREFIX} Trigger build #${CIRCLE_BUILD_NUM}" -m "${CIRCLE_BUILD_URL}"
+  git push origin "$branch"
+else
+  current_version=$(git describe --abbrev=0 --tags)
+  new_version=$(increment-version.sh "$current_version")
+  git tag -a "$new_version" -m "$new_version"
+  git push origin --tags
+fi
