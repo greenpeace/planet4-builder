089b9fce874609ae07f2ad5c2eb0c050257cc9b4
Nikos Roussos <nikos@roussos.cc>
2021-02-17 09:50:06 +0200

    Don't use full path on scripts
    Scripts are already in /usr/local/bin which is already in PATH

--- src/bin/notify-job-success.sh
+++ src/bin/notify-job-success.sh
@@ -5,5 +5,4 @@ MSG_TYPE="SUCCESS: ${TYPE:-Job} - ${CIRCLE_PROJECT_REPONAME} - ${CIRCLE_JOB}" \
   MSG_TITLE="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} @ ${CIRCLE_BRANCH:-${CIRCLE_TAG}}" \
   MSG_LINK="${CIRCLE_BUILD_URL}" \
   MSG_TEXT="Build: #${CIRCLE_BUILD_NUM} Diff: ${CIRCLE_COMPARE_URL} ${EXTRA_TEXT:-}" \
-  MSG_COLOUR="lightgreen" \
-  "${HOME}/scripts/notify-slack.sh"
+  MSG_COLOUR="lightgreen" "notify-slack.sh"
