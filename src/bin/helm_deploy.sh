9d47400cdf57aa645117cdd2468864c6b7795c44
gmintoco <50625416+gmintoco@users.noreply.github.com>
2022-05-09 14:43:24 +0200

    chore: remove redis workaround

--- src/bin/helm_deploy.sh
+++ src/bin/helm_deploy.sh
@@ -17,10 +17,6 @@ function install() {
   fi
   echo "FAILURE: Could not deploy release $HELM_RELEASE"
 
-  if grep -q "kind Service with the name \"${HELM_RELEASE}-redis-headless\" already exists in the cluster and wasn't defined in the previous release" helm_output.txt; then
-    kubectl -n "${HELM_NAMESPACE}" delete svc "${HELM_RELEASE}"-redis-headless
-  fi
-
   return 1
 }
 
