0eed9cb92b8e8540d018a4e107beff07c67a8e19
Nikos Roussos <nikos.roussos@greenpeace.org>
2022-10-14 15:15:25 +0300

    Check branch name for reseting test instances
    Since we stopped doing merge commits our previous check doesn't
    work anymore. We can just use `$CIRCLE_BRANCH` to determine if the
    pipeline runs on the main branch.

--- src/bin/check-pr-exists.sh
+++ src/bin/check-pr-exists.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-commit_msg="$1"
-
-# Check if this is a merge commit first, which means this triggered by a merged PR.
-# We use that check to reset the test instance.
+# Check if this is runs in the main branch. We use that check to reset the test instance.
 # Otherwise it's either a normal PR or just a new commit that doesn't require deployment.
-if [[ "$commit_msg" =~ ^Merge[[:space:]]pull[[:space:]]request[[:space:]]\#([[:digit:]]+) ]]; then
-  echo "https://github.com/greenpeace/${CIRCLE_PROJECT_REPONAME}/pull/${BASH_REMATCH[1]}" >/tmp/workspace/pr
-  echo "MERGED PR ID: ${BASH_REMATCH[1]}"
+if [[ "$CIRCLE_BRANCH" = "main" ]]; then
   echo true >/tmp/workspace/is_merge_commit
+  echo "$CIRCLE_PULL_REQUEST" >/tmp/workspace/pr
 else
   if [ -z "$CIRCLE_PULL_REQUEST" ]; then
     echo "No PR found, skipping instance deploy"
