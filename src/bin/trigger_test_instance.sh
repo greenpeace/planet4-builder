75aef34a77a49e9f3f085c32f1e3583fb4ea530d
Nikos Roussos <nikos.roussos@greenpeace.org>
2023-05-11 16:05:11 +0300

    Adjust reset trigger script to properly reset a test instance
    - Fully reset all repos regardless of the origin of the merged PR
    - Add reset keyword to trigger `make reset` step on test instance

--- src/bin/trigger_test_instance.sh
+++ src/bin/trigger_test_instance.sh
@@ -21,25 +21,36 @@ git clone "https://github.com/greenpeace/$instance_repo"
 
 composer_file="$instance_repo/composer-local.json"
 
-get_other_repos=".repositories // [] | map(select( .package.name != \"greenpeace/$repo\" ))"
+orig_json=$(cat "$composer_file")
 
-other_repos=$(jq "$get_other_repos" <"$composer_file")
-
+# If this is a merge commit fully reset the composer file and exit
 if [ "$is_merge_commit" == true ]; then
-  # If the workflow is running on the main branch, we set the version to the branch that was merged to.
-  version="dev-$CIRCLE_BRANCH"
-  package_repo=""
-else
-  version="7"
-  zip_url="https://storage.googleapis.com/$(cat /tmp/workspace/zip-path)"
-  composer_json=$(jq '{name,type,autoload,extra}' </tmp/workspace/composer.json |
-    jq ".version = $version" |
-    jq ".dist = {type: \"zip\", url: \"$zip_url\"}")
-  package_repo="{type: \"package\", package: $composer_json}"
+  echo "$orig_json" |
+    jq "del(.repositories)" |
+    jq --tab ".require.\"greenpeace/planet4-master-theme\" = \"dev-main\"" |
+    jq --tab ".require.\"greenpeace/planet4-plugin-gutenberg-blocks\" = \"dev-main\"" >"$composer_file"
+
+  git -C "$instance_repo" --no-pager diff
+  git -C "$instance_repo" add composer-local.json
+
+  git -C "$instance_repo" commit \
+    -m "[RESET] Reset instance" \
+    -m "Merged PR: ${CIRCLE_PULL_REQUEST:-$CIRCLE_PROJECT_REPONAME}"
+
+  git -C "$instance_repo" push
+  exit 0
 fi
 
-# This is needed since using cat directly when piping to the same file doesn't work here, resulted in a empty file.
-orig_json=$(cat "$composer_file")
+version="7"
+zip_url="https://storage.googleapis.com/$(cat /tmp/workspace/zip-path)"
+composer_json=$(jq '{name,type,autoload,extra}' </tmp/workspace/composer.json |
+  jq ".version = $version" |
+  jq ".dist = {type: \"zip\", url: \"$zip_url\"}")
+package_repo="{type: \"package\", package: $composer_json}"
+
+# Pick all other repos to clear
+get_other_repos=".repositories // [] | map(select( .package.name != \"greenpeace/$repo\" ))"
+other_repos=$(jq "$get_other_repos" <"$composer_file")
 
 echo "$orig_json" |
   jq ".repositories = $other_repos" |
@@ -47,7 +58,6 @@ echo "$orig_json" |
   jq --tab ".require.\"greenpeace/$repo\" = \"$version\"" >"$composer_file"
 
 git -C "$instance_repo" --no-pager diff
-
 git -C "$instance_repo" add composer-local.json
 
 git -C "$instance_repo" commit --allow-empty \
