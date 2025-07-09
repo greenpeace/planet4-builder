68f4136296fa9496d0ef15250fc95a383caff23c
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-07-09 15:14:53 +0300

    Add script for syncing elasticpress

---
+++ src/bin/elasticpress-sync.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu
+
+GCLOUD_ZONE=us-central1-a
+
+gcloud container clusters get-credentials "${GCLOUD_CLUSTER}" \
+  --zone "${GCLOUD_ZONE}" \
+  --project "${GOOGLE_PROJECT_ID}"
+
+php=$(kubectl get pods --namespace "${HELM_NAMESPACE}" \
+  --sort-by=.metadata.creationTimestamp \
+  --field-selector=status.phase=Running \
+  -l "release=${HELM_RELEASE},component=php" \
+  -o jsonpath="{.items[-1:].metadata.name}")
+
+if [[ -z "$php" ]]; then
+  echo "ERROR: PHP pod not found!"
+  exit 1
+fi
+
+kubectl --namespace "${HELM_NAMESPACE}" exec "$php" -- sh -c 'yes | wp wpml_elasticpress sync --setup'
+if [ $? -eq 1 ]; then
+  kubectl --namespace "${HELM_NAMESPACE}" exec "$php" -- sh -c 'wp elasticpress sync --setup --yes'
+fi
