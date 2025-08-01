c868db2356ff965962d2d09051d2fdb44297e465
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-08-01 20:23:54 +0300

    Adjust test instance booking script for Jira Cloud

--- src/bin/book-test-instance.py
+++ src/bin/book-test-instance.py
@@ -1,52 +1,27 @@
 #!/usr/bin/env python3
 
-import os
-import json
-import requests
-from requests.auth import HTTPBasicAuth
-from requests_oauthlib import OAuth1
-from oauthlib.oauth1 import SIGNATURE_RSA
-import re
 import argparse
-import hashlib
+from base64 import b64decode
+from jira import JIRA
+import json
+import os
+import re
+import sys
 
+from p4.apis import api_query
+from p4.github import (get_last_commit_date, get_repo_endpoints,
+                       get_pr_test_instance, has_open_pr_labeled_with_instance,
+                       add_issue_label)
 
-JIRA_API = 'https://jira.greenpeace.org/rest/api/2'
+JIRA_SERVER = 'https://greenpeace-planet4.atlassian.net/'
+JIRA_USER = os.getenv('JIRA_USER')
+JIRA_TOKEN = os.getenv('JIRA_TOKEN')
 GITHUB_API = 'https://api.github.com'
 SWARM_API = 'https://us-central1-planet-4-151612.cloudfunctions.net/p4-test-swarm'
-
 INSTANCE_REPO_PREFIX = 'greenpeace/planet4-test-'
-JIRA_INSTANCE_FIELD = 'customfield_13000'
 
 
-def get_pull_request(pr_url):
-    """
-    Fetch PR details
-
-    Extract data from PR url
-    Request PR data
-    """
-
-    regex = re.compile('https://github.com/(.*)/pull/([0-9]{1,6})')
-    matches = regex.match(pr_url)
-    vprint('Parsing URL {0}'.format(matches.groups()))
-
-    repository = matches.group(1) or None
-    pr_number = matches.group(2) or None
-
-    if not repository or not pr_number:
-        raise Exception('PR id could not be parsed.')
-
-    pr_endpoint = '{0}/repos/{1}/pulls/{2}'.format(
-        GITHUB_API,
-        repository,
-        pr_number
-    )
-
-    return api_query(pr_endpoint, {'Accept': 'application/vnd.github.v3+json'})
-
-
-def get_jira_issue(pr=None, jira_key=None):
+def get_jira_issue(pr=None):
     """
     Fetch Jira ticket
 
@@ -64,138 +39,46 @@ def get_jira_issue(pr=None, jira_key=None):
         # @todo: from commits
 
     if not jira_key:
-        raise Exception('Jira issue key could not be found.')
+        return None
 
-    vprint('Jira key found: {0}'.format(jira_key))
-    search_result = api_query(
-        JIRA_API + '/search?jql=key={0}'.format(jira_key))
-    if not search_result['issues']:
-        raise Exception(
-            'Issue could not be found with key {0}'.format(jira_key))
+    logs.append('Jira key found: {0}'.format(jira_key))
 
-    issue = search_result['issues'][0]
-    return {
-        'key': issue['key'],
-        'title': issue['fields']['summary'],
-        'state': issue['fields']['status']['name'],
-        'assignee': issue['fields']['assignee']['name'] if issue['fields']['assignee'] else None,
-        'test_instance': (issue['fields'][JIRA_INSTANCE_FIELD][0]['value']
-                          if issue['fields'][JIRA_INSTANCE_FIELD] else None),
-    }
+    token = b64decode(JIRA_TOKEN).decode('utf-8').replace('\n', '')
+    jira = JIRA(server=JIRA_SERVER, basic_auth=(JIRA_USER, token))
+
+    try:
+        issue = jira.issue(jira_key)
+    except:  # noqa: E722
+        print('No such ticket')
+        sys.exit(1)
+
+    return issue
 
 
-def book_instance(instance, jira_issue):
+def book_instance(instance, issue):
     """
-    Check jira ticket status & assigned instance
-    Move status if needed, book instance on ticket
+    Check jira issue status & assigned instance
+    Move status if needed, book instance on issue
 
-    @todo: make jira_issue a list of issues,
+    @todo: make issue a list of issues,
     loop through it to be able to lock multiple issues to one instance & pr
     """
 
-    current_state = jira_issue['state']
-    editable_status = ['Open', 'Validated']
-    invalid_status = ['Closed']
+    current_state = issue.fields.status.name
+    invalid_status = ['Closed', 'Open']
 
     if current_state in invalid_status:
         raise Exception(
             'Issue is in an invalid state <{0}>, stopping process.'.format(current_state))
 
-    # Transition issue to valid status
-    if current_state in editable_status:
-        transition_issue(jira_issue)
-    else:
-        vprint('Issue is not in an editable status <{0}>, skipping transition.'.format(
-            current_state
-        ))
-
     # Fill <instance> field
-    if not jira_issue['test_instance'] or jira_issue['test_instance'] != instance:
-        edit_issue(jira_issue, instance)
+    if (not issue.fields.customfield_10201 or issue.fields.customfield_10201[0].value != instance):
+        issue.update(fields={'customfield_10201': [{'value': instance}]})
     else:
-        vprint('Issue is already configured for instance ({0}), skipping configuration.'.format(
-            instance))
+        logs.append('Issue is already configured for instance ({0}),'
+                    ' skipping configuration.'.format(instance))
 
     return True
-
-
-def transition_issue(jira_issue):
-    if not jira_auth:
-        raise Exception('Issue cannot be modified without Jira credentials.')
-
-    in_dev = 'IN DEVELOPMENT'
-    transition_endpoint = '{0}/issue/{1}/transitions'.format(
-        JIRA_API, jira_issue['key'])
-    response = api_query(transition_endpoint, auth=jira_auth['auth'])
-
-    available_transitions = list(map(
-        lambda s: {'id': s['id'], 'status': s['to']
-                   ['name'], 'name': s['name']},
-        response['transitions']
-    ))
-    vprint(available_transitions)
-    dev_transition = list(
-        filter(lambda t: t['status'] == in_dev, available_transitions))[0]
-
-    # Transition not available
-    if not dev_transition:
-        return True
-
-    # Transition
-    data = {'transition': {'id': dev_transition['id']}}
-
-    vprint('POST {0}\n{1}'.format(transition_endpoint, json.dumps(data, indent=4)))
-
-    if dryrun:
-        return True
-
-    response = requests.post(transition_endpoint,
-                             auth=jira_auth['auth'],
-                             data=json.dumps(data),
-                             headers={
-                                 'Content-type': 'application/json',
-                                 'Accept': 'application/json'
-                             })
-
-    vprint('Transitioned issue, response: ')
-    vprint(response.status_code, response.headers, response.text)
-
-    failed = api_failed(response, transition_endpoint, exit_on_error=False)
-    if failed:
-        vprint('Status transition failed, please move the issue manually in Jira.')
-        return False
-
-    return True
-
-
-def edit_issue(jira_issue, instance):
-    if not jira_auth:
-        raise Exception('Issue cannot be modified without Jira credentials.')
-
-    endpoint = '{0}/issue/{1}'.format(JIRA_API, jira_issue['key'])
-    data = {'fields': {JIRA_INSTANCE_FIELD: [{'value': instance}], }}
-
-    if dryrun:
-        vprint("PUT {0}\n{1}".format(endpoint, json.dumps(data, indent=4)))
-        return True
-
-    response = requests.put(endpoint,
-                            auth=jira_auth['auth'],
-                            data=json.dumps(data),
-                            headers={'Content-type': 'application/json'})
-    failed = api_failed(response, endpoint, exit_on_error=False)
-    if failed:
-        raise Exception(
-            'Issue could not be edited, instance booking process has failed.')
-
-    return True
-
-
-"""
-
-Instances
-
-"""
 
 
 def get_available_instance():
@@ -205,15 +88,27 @@ def get_available_instance():
     return oldest available instance
     """
     instances = get_instances()
+    logs.append('Swarm response {0}'.format(json.dumps(instances, indent=4)))
 
     available_list = list(filter(lambda name: instances[name] == 1, instances))
+
+    if not len(available_list):
+        raise Exception('No available instance could be found.')
+
+    not_used_with_label = list(
+        filter(
+            lambda name: not has_open_pr_labeled_with_instance(name, logs),
+            available_list
+        ))
+
     dated_list = list(
-        map(lambda name: [name, get_instance_last_commit_date(name)], available_list))
+        map(lambda name: [name, get_last_commit_date(INSTANCE_REPO_PREFIX + name)],
+            not_used_with_label))
 
     dated_list.sort(key=lambda i: i[1])
 
-    if not dated_list[0][0]:
-        raise Exception('No available instance could be found.')
+    if not len(dated_list):
+        raise Exception('No available instance after checking Github labels.')
 
     return dated_list[0][0]
 
@@ -227,110 +122,10 @@ def get_instances():
     return api_query(SWARM_API)
 
 
-def get_instance_last_commit_date(instance):
-    """
-    Return last commit date for an instance repo
-    """
-    commit = api_query(
-        GITHUB_API + '/repos/' + INSTANCE_REPO_PREFIX + instance + '/commits/main',
-        {'Accept': 'application/vnd.github.v3+json'}
-    )
-
-    return commit['commit']['committer']['date']
-
-
-"""
-
-API stuff
-
-"""
-
-
-def api_query(url, headers={'Accept': 'application/json'}, auth=None):
-    """
-    Queries API
-    - fails on error
-    - return json
-    - use cache if requested
-    """
-
-    vprint('GET {0}'.format(url))
-
-    cache_file = '/tmp/{0}.cache'.format(hashlib.md5(url.encode()).hexdigest())
-    if use_request_cache and os.path.isfile(cache_file):
-        vprint('Using cache for ' + url)
-        return json.load(open(cache_file))
-
-    response = requests.get(url, headers=headers, auth=auth)
-    api_failed(response, url)
-
-    content = response.json()
-
-    if use_request_cache:
-        vprint(url, ' -> ', cache_file)
-
-    json.dump(content, open(cache_file, "w"))  # write anyway
-    return content
-
-
-def api_failed(response, endpoint, exit_on_error=True):
-    """
-    Check if api request failed
-
-    Can raise exception
-    """
-
-    if 200 <= response.status_code < 300:
-        return False
-
-    vprint('API call failed')
-    vprint(response.status_code, response.headers, response.text)
-    if exit_on_error:
-        raise Exception("Status code {0} calling {1}".format(
-            response.status_code, endpoint))
-    return True
-
-
-def get_jira_auth():
-    """
-    Based on env variables
-
-    https://developer.atlassian.com/server/jira/platform/oauth/
-    """
-
-    if os.getenv('JIRA_CLIENT_KEY'):
-        if os.getenv('JIRA_PRIVATE_KEY'):
-            key = os.getenv('JIRA_PRIVATE_KEY')
-        else:
-            key = None
-
-        return {
-            'type': 'oauth',
-            'auth': OAuth1(
-                os.getenv('JIRA_CLIENT_KEY'),
-                resource_owner_key=os.getenv('JIRA_OAUTH_TOKEN'),
-                resource_owner_secret=os.getenv('JIRA_OAUTH_SECRET'),
-                rsa_key=key,
-                signature_method=SIGNATURE_RSA,
-            )
-        }
-
-    return {
-        'type': 'basic',
-        'auth': HTTPBasicAuth(os.getenv('JIRA_USERNAME'), os.getenv('JIRA_PASSWORD'))
-    } if os.getenv('JIRA_USERNAME') else {}
-
-
 def save_results(results, filename='booking-results.json'):
     with open(filename, 'w') as results_file:
         json.dump(results, results_file, indent=2)
 
-
-"""
-
-Main
-
-"""
 
 if __name__ == '__main__':
 
@@ -340,8 +135,6 @@ if __name__ == '__main__':
                         help="pull request URL")
     parser.add_argument("-n", "--dryrun", action="store_true",
                         help="gives a course of action but doesn't execute")
-    parser.add_argument("-v", "--verbosity", action="count", default=0,
-                        help="increase output verbosity")
     parser.add_argument("--no-cache", action="store_true",
                         help="Disable request cache use")
     parser.add_argument("--no-booking", action="store_true",
@@ -354,54 +147,54 @@ if __name__ == '__main__':
     # Parsed options
     pr_url = args.pr_url if args.pr_url else os.getenv('CIRCLE_PULL_REQUEST')
     dryrun = args.dryrun
-    verbose = args.verbosity
-    use_request_cache = not args.no_cache
     results_file = args.results
+
     # Logs
     logs = []
 
-    def vprint(*args):
-        for msg in args:
-            logs.append(msg)
-            if verbose:
-                print(msg)
-    # Auth
-    jira_auth = get_jira_auth()
-
     # Main program
 
-    vprint('# Running for {0}'.format(pr_url))
+    logs.append('# Running for {0}'.format(pr_url))
     if dryrun:
-        vprint('## Dry run, nothing will be commited.')
+        logs.append('## Dry run, nothing will be commited.')
 
     # Fetch PR details
-    pr = get_pull_request(pr_url=pr_url)
-    if not pr:
+    pr_endpoint, _ = get_repo_endpoints(pr_url=pr_url)
+    if not pr_endpoint:
         raise Exception('No pull request found, aborting.')
 
+    pr = api_query(pr_endpoint)
+
     # Fetch issue details from Github PR
-    try:
-        issue = get_jira_issue(pr=pr)
-    except Exception as e:
-        vprint(e)
-        issue = None
+    issue = get_jira_issue(pr=pr)
 
-    # Define instance
+    # If not an issue PR just get an available instance
+    # Reverse the order to avoid race condition
+    instance = None
     if not issue:
-        raise Exception('No corresponding issue found, booking will not be executed.')
-
-    if issue['test_instance']:
-        instance = issue['test_instance']
-        vprint('Issue is already deployed on {0}, reusing.'.format(instance))
+        instance = get_pr_test_instance(pr_endpoint)
+        issue_key = None
     else:
-        instance = get_available_instance()
+        # Use pre-booked instance or get a new one
+        test_instance = None
+        issue_key = issue.key
+        if issue.fields.customfield_10201:
+            test_instance = issue.fields.customfield_10201[0].value
+        if test_instance:
+            instance = test_instance
+            logs.append('Issue is already deployed on {0}, reusing.'.format(instance))
 
-    book_instance(instance, issue)
+    if not instance:
+        instance = get_available_instance()
+        label_name = '[Test Env] {0}'.format(instance)
+        add_issue_label(pr_endpoint, label_name)
+        if issue:
+            book_instance(instance, issue)
 
     if results_file:
         save_results({
             'instance': instance,
-            'issue': issue,
+            'issue': issue_key,
             'pr': pr,
             'logs': logs,
         }, results_file)
